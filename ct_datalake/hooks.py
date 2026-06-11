709f4c4a8ab891c4826904ecfb6f3402ce9c9154
Le Thanh Anh <leejay3636@gmail.com>
2026-06-11 15:05:46 +0700

    feat: add www portal pages and update recent changes

--- ct_datalake/hooks.py
+++ ct_datalake/hooks.py
@@ -7,3 +7,10 @@ app_license = "MIT"
 
 # Expose API
 override_whitelisted_methods = {}
+
+
+# SPA Routing
+website_route_rules = [
+    {"from_route": "/aicenter/2as-master-profile/<path:app_path>", "to_route": "ct_datalake_spa"},
+    {"from_route": "/aicenter/2as-master-profile", "to_route": "ct_datalake_spa"}
+]
